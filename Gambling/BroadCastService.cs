75d75e814973369f5a4ebdb4705b1c3cc41a1c8b
ul-rotha <51068059+ul-rotha@users.noreply.github.com>
2022-05-29 20:14:33 +0700

    update_signalr_random_loto

---
+++ Gambling/BroadCastService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Gambling
+{
+    enum PlayGame
+    {
+        StartGame,
+        EndGame,
+        Waiting
+    }
+
+    public class BroadCastService : BackgroundService
+    {
+        private PlayGame playGame = PlayGame.Waiting;
+
+        private IHubContext<ChatHub> hub;
+        public BroadCastService(IHubContext<ChatHub> _hub)
+        {
+            hub = _hub;
+        }
+        const int second = 5;
+        int i = second;
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Delay(5000);
+            while (!stoppingToken.IsCancellationRequested)
+            {   
+                await Task.Delay(1000);
+                
+                if (playGame  == PlayGame.StartGame)
+                {
+                    //call start game
+                    await hub.Clients.All.SendAsync("ReceiveMessage", "startGame", "");
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+
+
+                    //call random main play
+                    await hub.Clients.All.SendAsync("ReceiveMessage", "mainResult", "2");
+
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+
+                    //call random second play
+                    await hub.Clients.All.SendAsync("ReceiveMessage", "subPlay", "");
+
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+
+                    await hub.Clients.All.SendAsync("ReceiveMessage", "subResult", "2");
+
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+
+                    playGame = PlayGame.Waiting;
+                    i = second + 1;
+
+                }
+                else if(playGame == PlayGame.Waiting)
+                {
+
+                    if (i == 0)
+                    {
+                        await hub.Clients.All.SendAsync("ReceiveMessage", "countdown", i);
+                        playGame = PlayGame.StartGame;
+                    }
+                    else
+                    {
+                        await hub.Clients.All.SendAsync("ReceiveMessage", "countdown", i);
+                    }
+                }
+                i--;
+            }
+        }
+    }
+}
--- Gambling/wwwroot/js/chat.js
+++ Gambling/wwwroot/js/chat.js
@@ -5,6 +5,34 @@
 
 connection.on("ReceiveMessage", function (user, message) {
     console.log(user);
+    if (user == "startGame") {
+        StartGame();
+    }
+    else if (user == "mainResult") {
+        mainTimer.stop();
+        $(".main").removeClass("lotto-ball-active");
+        var id = "#mainResult" + message;
+        result = message;
+        $(id).addClass("lotto-ball-active");
+    }
+    else if (user == "subPlay") {
+        StartSubGame();
+    }
+    else if (user == "subResult") {
+        subTimer.stop();
+        var id = "sub-play" + message;
+        
+        $("." + id).removeClass("lotto-ball-sub-active");
+        console.log("." + id + "[key=" + message + "]");
+        $("." + id + "[key=" + message + "]").addClass("lotto-ball-sub-active");
+    }
+    else if (user == "countdown") {
+        $(".main").removeClass("lotto-ball-active");
+        $(".sub-play1").removeClass("lotto-ball-sub-active");
+        $(".sub-play2").removeClass("lotto-ball-sub-active");
+        $(".sub-play3").removeClass("lotto-ball-sub-active");
+        countdown(message);
+    }
 });
 
 connection.start().then(function () {
@@ -14,11 +42,31 @@ connection.start().then(function () {
     return console.error(err.toString());
 });
 
-//document.getElementById("sendButton").addEventListener("click", function (event) {
-//    var user = document.getElementById("userInput").value;
-//    var message = document.getElementById("messageInput").value;
-//    connection.invoke("SendMessage", user, message).catch(function (err) {
-//        return console.error(err.toString());
-//    });
-//    event.preventDefault();
-//});
+
+
+var totalminute = 0;
+var timespent = 0;
+function countdown(timeremaining) {
+
+    console.log("start count:" + timeremaining);
+
+    totalminute = parseInt((timeremaining) / 60);
+
+
+
+    var seconds = timeremaining - totalminute * 60;
+
+    if (seconds < 0) {
+        $("#countdown").html("0");
+
+    } else {
+        if (seconds < 10) {
+            $("#countdown").html('0' + totalminute + ":0" + seconds + "");
+        } else {
+            $("#countdown").html('0' + totalminute + ":" + seconds + "");
+        }
+
+        // $("#div_timer").html(timeremaining);
+    }
+
+}
