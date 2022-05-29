a058c274ae9731ddaa032f27f47238777b6f7f8d
ul-rotha <51068059+ul-rotha@users.noreply.github.com>
2022-05-29 08:59:40 +0700

    first initial

---
+++ Gambling/Pages/Index_win.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gambling.Pages
+{
+    public class Index_winModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public Index_winModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Gambling/wwwroot/js/chat.js
@@ -0,0 +1,24 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable the send button until connection is established.
+//document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    console.log(user);
+});
+
+connection.start().then(function () {
+    console.log("Stated");
+    //document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+//document.getElementById("sendButton").addEventListener("click", function (event) {
+//    var user = document.getElementById("userInput").value;
+//    var message = document.getElementById("messageInput").value;
+//    connection.invoke("SendMessage", user, message).catch(function (err) {
+//        return console.error(err.toString());
+//    });
+//    event.preventDefault();
+//});
