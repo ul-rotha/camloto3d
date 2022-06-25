a5524d3574a9ba2679cc6451a9d1b5219b7f21c9
chanpheakdey <chanpheakdey.vong@gmail.com>
2022-06-25 13:49:43 +0700

    add history

---
+++ Gambling/Pages/history.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalR.Pages
+{
+    public class historyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
