d135630aa4e49331fae8adcb311b254dddd01451
chanpheakdey <chanpheakdey.vong@gmail.com>
2022-06-25 11:32:29 +0700

    Add Login and Report

---
+++ Gambling/Pages/user.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalR.Pages
+{
+    public class userModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
