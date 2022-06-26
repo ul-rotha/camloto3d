5ae1a50d7fc897ab347b1025b5d7ee20ae52b1c8
chanpheakdey <chanpheakdey.vong@gmail.com>
2022-06-26 07:47:55 +0700

    fixed selected date error

---
+++ Gambling/Pages/scanresult.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalR.Pages
+{
+    public class ScanResultModel : PageModel
+    {
+        private readonly ILogger<ScanResultModel> _logger;
+
+        public ScanResultModel(ILogger<ScanResultModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
